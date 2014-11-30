b430155d9624463985ef9dfbe06fda21ae44793b
hi@prabros.com <hi@prabros.com>
2014-11-30 20:27:40 +0000

    Fix Exercise 1.3.rkt

--- exercises/chapter-1/Exercise 1.3.rkt
+++ exercises/chapter-1/Exercise 1.3.rkt
@@ -2,11 +2,14 @@
 
 (define (max a b) (if (> a b) a b))
 
+(define (min a b) (if (< a b) a b))
+
 (define (square a) (* a a))
 
 (define (square-of-large a b c)
   (+ (square (max a b))
-     (square (max (max a b) c))))
+     (square (max (min a b) c))))
 
 (square-of-large 2 3 4)
 
+(square-of-large 4 3 2)
